53e17f91870fb0816e3a5c9967cd06e49061f63f
Jakub <rutrle@seznam.cz>
2021-06-26 10:06:40 +0200

    url for login page

--- weather_web/weather_web/urls.py
+++ weather_web/weather_web/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('weather.urls', namespace='weather'))
+    path('', include('weather.urls', namespace='weather')),
+    path('account/', include('accounts.urls', namespace='accounts'))
 ]
