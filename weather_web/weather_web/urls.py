1d4d108ce46fe7d5a754ceab37554edcb75d8192
Jakub <rutrle@seznam.cz>
2021-07-16 13:19:15 +0200

    added  bootstrap4

--- weather_web/weather_web/urls.py
+++ weather_web/weather_web/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('weather.urls', namespace='weather')),
-    path('account/', include('accounts.urls', namespace='accounts'))
+    path('account/', include('accounts.urls', namespace='accounts')),
+    path('account/', include("django.contrib.auth.urls"))
 ]
