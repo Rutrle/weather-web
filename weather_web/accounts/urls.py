7a355a9aef5d43d3fa0d023ba8295cd9a87f01e2
Jakub <rutrle@seznam.cz>
2021-09-01 00:10:31 +0200

    added logout to navbar

--- weather_web/accounts/urls.py
+++ weather_web/accounts/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', SignUp.as_view(), name='signup'),
-
 ]
