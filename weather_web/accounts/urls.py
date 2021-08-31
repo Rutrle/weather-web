6b673040b827e5f50deec2a0ea22ebadea334dc1
Jakub <rutrle@seznam.cz>
2021-09-01 00:47:44 +0200

    reworked logout and login view,  work in progress on login

--- weather_web/accounts/urls.py
+++ weather_web/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import SignUp
+from .views import SignUp, login_view, logout_view
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view,
+         name='logout'),
     path('signup/', SignUp.as_view(), name='signup'),
 ]
--- weather_web/accounts/views.py
+++ weather_web/accounts/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 from . import forms
 from django.views.generic import CreateView
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
 class SignUp(CreateView):
     form_class = forms.UserCreateForm
     success_url = reverse_lazy("login")
     template_name = "accounts/signup.html"
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('weather:weather_index'))
+
+        else:
+            return HttpResponseRedirect(reverse('weather:weather_index'))
+    else:
+        return render(request, 'accounts/login.html', context={})
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('weather:weather_index'))
