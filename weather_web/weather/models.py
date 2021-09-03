1e1c2556fcd9f50e12a6d76ce494dec93223a92a
Jakub <rutrle@seznam.cz>
2021-09-03 20:35:52 +0200

    improved visuals of places list

--- weather_web/weather/models.py
+++ weather_web/weather/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 class Place(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    # author = models.ForeignKey(
+    #    'accounts.User', related_name='username', on_delete=models.SET_NULL, null=True)
     longtitude = models.DecimalField(max_digits=8, decimal_places=6)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
 
