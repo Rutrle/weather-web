3e82bd03a59b98c5f30b8cd689d2e08a2d85f21c
Jakub <rutrle@seznam.cz>
2021-09-03 23:41:45 +0200

    added link to place create to place list

--- weather_web/weather/views.py
+++ weather_web/weather/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from .models import Place
 from .forms import PlaceForm
 from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class TestBaseView(generic.TemplateView):
@@ -13,12 +14,22 @@ class WeatherIndexView(generic.TemplateView):
     template_name = 'weather/weather_index.html'
 
 
-class PlaceCreateView(generic.CreateView):
-    form_class = PlaceForm
+class PlaceCreateView(generic.CreateView, LoginRequiredMixin):
+    #form_class = PlaceForm
+    model = Place
+    fields = ('name', 'longtitude', 'latitude')
+
     template_name = 'weather/place_create.html'
 
     def get_success_url(self):
         return reverse('weather:weather_index')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        print(self.request.user)
+        self.object.author = self.request.user
+        self.object.save()
+        return super().form_valid(form)
 
 
 class PlaceListView(generic.ListView):
