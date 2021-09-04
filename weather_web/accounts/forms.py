1a60d87a374fe380f21763afc5de0b09fb74a082
Jakub <rutrle@seznam.cz>
2021-09-04 09:45:45 +0200

    changed user model to auth one

--- weather_web/accounts/forms.py
+++ weather_web/accounts/forms.py
@@ -7,7 +7,7 @@ from .models import User
 class UserCreateForm(UserCreationForm):
     class Meta:
         fields = ("username", "email", "password1", "password2")
-        model = User
+        model = get_user_model()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- weather_web/weather/models.py
+++ weather_web/weather/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from accounts import models as account_models
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class Place(models.Model):
     name = models.CharField(max_length=255, unique=True)
     author = models.ForeignKey(
-        account_models.User, related_name='%(class)susername', on_delete=models.SET_NULL, null=True)
+        get_user_model(), related_name='%(class)susername', on_delete=models.SET_NULL, null=True)
     longtitude = models.DecimalField(max_digits=8, decimal_places=6)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     created_date = models.DateTimeField(default=timezone.now)
