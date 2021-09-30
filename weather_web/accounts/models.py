542a8a019902e361b19d8441f766dbdb4e1cef75
Jakub <rutrle@seznam.cz>
2021-10-01 01:23:28 +0200

    created user preference model

--- weather_web/accounts/models.py
+++ weather_web/accounts/models.py
@@ -8,11 +8,14 @@ class User(auth.models.User, auth.models.PermissionsMixin):
     def __str__(self):
         return "@{}".format(self.username)
 
-''' 
-class UserPreference():
+
+class UserPreference(models.Model):
+    degrees_choices = [('C', 'Celsius'),
+                       ('F', 'Fahrenheit')
+                       ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    degrees = models.Choices()
+    degrees = models.CharField(
+        max_length=255, choices=degrees_choices, default='C')
 
     def __str__(self):
         return self.user.username + '_preferences'
-'''
